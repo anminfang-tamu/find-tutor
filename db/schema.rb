3ed1c799a1637de21599fc6abbb15bea456ccac8
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-03-09 21:53:32 +0000

    Created users table through migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20220309214916) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.string   "user_name"
+    t.string   "display_name"
+    t.text     "description"
+    t.string   "major"
+    t.string   "tutor_major"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
